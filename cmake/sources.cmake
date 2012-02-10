e5d1e7433855dd66718864132e7333cf57411aff
Error323 <folkerthuizinga@gmail.com>
2012-02-10 16:27:04 +0100

    Added basic main files

--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -2,12 +2,15 @@
 
 # === Emulator sources
 set(EMULATOR_SOURCES
+  src/emulator/main.cpp
 )
 
 # === Server sources
 set(SERVER_SOURCES
+  src/server/main.cpp
 )
 
 # === Pipeline sources
 set(PIPELINE_SOURCES
+  src/pipeline/main.cpp
 )
