1728de939fdd39321512d3d9f1a55fb00c78e33b
Error323 <folkerthuizinga@gmail.com>
2012-02-11 17:20:08 +0100

    Set application stuff

--- src/emulator/main.cpp
+++ src/emulator/main.cpp
@@ -1,4 +1,5 @@
 #include "UniboardEmulator.h"
+#include "version.h"
 
 #include <pelican/emulator/EmulatorDriver.h>
 #include <pelican/utility/ConfigNode.h>
@@ -9,10 +10,17 @@ int main(int argc, char* argv[])
 {
   QCoreApplication app(argc, argv);
 
+  app.setApplicationName(HUMAN_NAME);
+  app.setApplicationVersion(VERSION);
+  app.setOrganizationName("Anton Pannekoek Institute");
+  app.setOrganizationDomain("http://www.aartfaac.org");
+
   if (argc != 2)
   {
     qFatal("Usage: %s <table dir>", argv[0]);
   }
+
+  qDebug("%s", HUMAN_NAME);
 
   pelican::ConfigNode xmlNode(
         "<UniboardEmulator>"
