ef94bcfbc04bfa504856aff5113e04e1aa61f173
John Swinbank <swinbank@transientskp.org>
2012-04-03 16:30:38 +0200

    Much simpler FindPelican.cmake

--- cmake/modules/FindPelican.cmake
+++ cmake/modules/FindPelican.cmake
@@ -6,20 +6,12 @@
 # Dependencies for Pelican searched for
 # - Boost program options
 # - Qt (Core, Xml, Network, Gui)
-# - CppUnit
 #
 # =============================================================================
 # Defines the following variables:
 #
 #   PELICAN_FOUND             = True if Pelican is found.
-#
-#   PELICAN_CMAKE_MODULE_DIR  = Pelican CMake module path.
-#
-#   PELICAN_INCLUDE_DIR       = Top level Pelican include directory.
 #   PELICAN_INCLUDES          = Set of include directories needed by Pelican.
-#
-#   PELICAN_LIBRARY           = The Pelican library.
-#   PELICAN_TESTUTILS_LIBRARY = The Pelican test utility library.
 #   PELICAN_LIBRARIES         = Set of libraries required for linking.
 #
 # ============================================================================
@@ -35,117 +27,56 @@
 #   - This script is untested with either Mac OS or Windows.
 #
 # ============================================================================
-# Last Update: 26 Oct 2010
-# =============================================================================
 
+if (NOT PELICAN_FOUND)
+    # Include default error handling macro.
+    include(FindPackageHandleStandardArgs)
 
-# Include default error handling macro.
-include(FindPackageHandleStandardArgs)
+    # Find the top level Pelican include directory.
+    find_path(PELICAN_INCLUDES pelican
+        HINTS
+            ${PELICAN_ROOT}
+            $ENV{PELICAN_ROOT}
+        PATH_SUFFIXES
+            include
+    )
 
+    # Find the Pelican library.
+    find_library(PELICAN_LIBRARIES pelican
+        HINTS
+            ${PELICAN_ROOT}
+            $ENV{PELICAN_ROOT}
+        PATH_SUFFIXES
+            lib
+            lib64
+    )
 
-# Find the top level Pelican include directory.
-find_path(PELICAN_INCLUDE_DIR pelican
-    PATHS
-    ${PELICAN_ROOT}/include
-    $ENV{PELICAN_ROOT}/include
-    /usr/include
-    /usr/local/include
-)
-set(PELICAN_INCLUDES ${PELICAN_INCLUDE_DIR})
+    # Check for errors.
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+        Pelican
+        "ERROR: Could not find Pelican include directory."
+        PELICAN_INCLUDES
+    )
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+        Pelican
+        "ERROR: Could not find Pelican library."
+        PELICAN_LIBRARIES
+    )
 
+    # Find dependencies.
+    find_package(Boost COMPONENTS program_options REQUIRED)
+    find_package(Qt4 COMPONENTS QtCore QtNetwork QtXml REQUIRED)
+    list(APPEND PELICAN_INCLUDES
+        ${Boost_PROJECT_OPTIONS_INCLUDE_DIR}
+        ${QT_INCLUDE_DIR}
+        ${QT_QTCORE_INCLUDE_DIR}
+    )
 
-# Find the Pelican library.
-find_library(PELICAN_LIBRARY pelican
-    NAMES pelican
-    PATHS
-    ${PELICAN_ROOT}/lib
-    $ENV{PELICAN_ROOT}/lib
-    /usr/lib
-    /usr/local/lib
-)
-set(PELICAN_LIBRARIES ${PELICAN_LIBRARY})
+    # Put variables in the advanced section of the CMake cache.
+    mark_as_advanced(
+        PELICAN_FOUND
+        PELICAN_INCLUDES
+        PELICAN_LIBRARIES
+    )
 
-
-# Find the Pelican test utility library.
-find_library(PELICAN_TESTUTILS_LIBRARY pelican-testutils
-    NAMES
-    pelican-testutils
-    PATHS
-    ${PELICAN_ROOT}/lib
-    $ENV{PELICAN_ROOT}/lib
-    /usr/lib
-    /usr/local/lib
-)
-list(APPEND PELICAN_LIBRARIES ${PELICAN_TESTUTILS_LIBRARY})
-
-
-# Find Pelican cmake modules.
-find_path(PELICAN_CMAKE_MODULE_DIR FindPelicanInstall.cmake
-    PATHS
-    ${PELICAN_ROOT}/share/pelican/cmake
-    $ENV{PELICAN_ROOT}/share/pelican/cmake
-    /usr/
-    /usr/share
-    /usr/share/pelican
-    /usr/share/pelican/cmake
-    /usr/local/
-    /usr/local/share
-    /usr/local/share/pelican
-    /usr/local/share/pelican/cmake
-    PATH_SUFFIXES
-    share
-    cmake
-    pelican
-    DOC
-    "Location of Pelican cmake modules."
-)
-
-
-# Check for errors.
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pelican
-    "ERROR: Could not find Pelican include directory."
-    PELICAN_INCLUDE_DIR
-)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pelican
-    "ERROR: Could not find Pelican library."
-    PELICAN_LIBRARY
-)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pelican
-    "ERROR: Could not find Pelican test utility library."
-    PELICAN_TESTUTILS_LIBRARY
-)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pelican
-    "ERROR: Could not find Required Pelican cmake module path"
-    PELICAN_CMAKE_MODULE_DIR
-)
-
-
-# Add the Pelican CMake modules into the CMake module path.
-list(INSERT CMAKE_MODULE_PATH 0 "${PELICAN_CMAKE_MODULE_DIR}")
-list(INSERT CMAKE_INCLUDE_PATH 0 "${PELICAN_CMAKE_MODULE_DIR}")
-
-
-
-# Find dependencies.
-find_package(Boost COMPONENTS program_options REQUIRED)
-find_package(Qt4 COMPONENTS QtCore QtNetwork QtXml REQUIRED)
-find_package(CppUnit REQUIRED)
-list(APPEND PELICAN_INCLUDES
-    ${Boost_PROJECT_OPTIONS_INCLUDE_DIR}
-    ${QT_INCLUDE_DIR}
-    ${QT_QTCORE_INCLUDE_DIR}
-)
-include_directories(${PELICAN_INCLUDES})
-
-
-# Put variables in the advanced section of the CMake cache.
-mark_as_advanced(
-    PELICAN_FOUND
-    PELICAN_CMAKE_MODULE_DIR
-    PELICAN_INCLUDE_DIR
-    PELICAN_INCLUDES
-    PELICAN_LIBRARY
-    PELICAN_TESTUTILS_LIBRARY
-    PELICAN_LIBRARIES
-)
-
+endif (NOT PELICAN_FOUND)
