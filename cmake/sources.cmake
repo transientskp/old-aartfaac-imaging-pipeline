f24134a02c1f50b18032d74230c45952d19cd33f
Error323 <folkerthuizinga@gmail.com>
2012-04-16 17:10:20 +0200

    Add CasaImageStorage container

--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -16,6 +16,7 @@ set(PIPELINE_SOURCES
   src/pipeline/UniboardAdapter.cpp
   src/pipeline/UniboardPipeline.cpp
   src/pipeline/modules/output/TiffStorage.cpp
+  src/pipeline/modules/output/CasaImageStorage.cpp
   src/pipeline/modules/calibrator/Calibrator.cpp
   src/pipeline/modules/imager/Imager.cpp
   src/utilities/halfprecision.c
---
+++ src/pipeline/modules/output/CasaImageStorage.cpp
@@ -0,0 +1,38 @@
+#include "CasaImageStorage.h"
+#include "../../UniboardDataBlob.h"
+
+#include <casacore/images/Images/PagedImage.h>
+#include <QtCore>
+
+CasaImageStorage::CasaImageStorage(const ConfigNode &inConfigNode)
+  : AbstractOutputStream(inConfigNode)
+{
+  mPath = inConfigNode.getOption("file", "path", "./");
+}
+
+CasaImageStorage::~CasaImageStorage()
+{
+
+}
+
+void CasaImageStorage::sendStream(const QString &inStreamName, const DataBlob *inDataBlob)
+{
+  Q_UNUSED(inStreamName);
+
+  const UniboardDataBlob *blob = static_cast<const UniboardDataBlob*>(inDataBlob);
+
+  if (blob->type() != "UniboardDataBlob")
+  {
+    qWarning("Expected datablob type 'UniboardDataBlob', got '%s' ignoring...", qPrintable(blob->type()));
+    return;
+  }
+
+  const std::vector<float> &skymap = blob->getSkyMap();
+  QString filename = mPath + "/" +
+      QString::number(blob->getFrequency(), 'f', 6) +
+      "_" + blob->getDateTime().toString("dd-MM-yyyy_hh:mm:ss") + ".image";
+
+  Q_UNUSED(skymap);
+  Q_UNUSED(filename);
+}
+
