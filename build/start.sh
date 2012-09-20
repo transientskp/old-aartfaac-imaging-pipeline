43e0a46734cd6dafc035175f69b28d1c539e5ab4
Error323 <folkerthuizinga@gmail.com>
2012-09-20 13:54:56 +0200

    Added start/stop script for imaging

---
+++ build/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+ROOT=`pwd`
+SCONFIG=${ROOT}/../src/xml/serverConfig.xml
+PCONFIG=${ROOT}/../src/xml/pipelineConfig.xml
+PIPELINES=5
+
+if [ ! -d "$1" ]; then
+	echo "ERROR: '$1' should point to a measurement set." >&2
+	exit 1
+fi
+
+echo "Starting aartfaac-server"
+${ROOT}/aartfaac-server ${SCONFIG} &
+sleep 2
+
+echo "Starting ${PIPELINES} aartfaac pipelines"
+for (( i=0; i<${PIPELINES}; i++ )); do
+	${ROOT}/aartfaac-pipeline ${PCONFIG} &
+done;
+sleep 5
+
+echo "Starting aartfaac emulator"
+${ROOT}/aartfaac-emulator $1 &
+
+exit 0
---
+++ build/stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkill -U ${USER} aartfaac
