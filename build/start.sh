5bf7027b032ca2e00e2b101b98e0940a96d6d870
Error323 <folkerthuizinga@gmail.com>
2012-11-09 14:25:57 +0100

    Update CMakeLists.txt

--- build/start.sh
+++ build/start.sh
@@ -2,27 +2,64 @@
 
 set -e
 
-ROOT=`pwd`
-SCONFIG=${ROOT}/../src/xml/serverConfig.xml
-PCONFIG=${ROOT}/../src/xml/pipelineConfig.xml
-PIPELINES=5
+usage()
+{
+cat << EOF
+usage: `basename $0` options
 
-if [ ! -d "$1" ]; then
-	echo "ERROR: '$1' should point to a measurement set." >&2
-	exit 1
+This script starts an imaging pipeline.
+
+OPTIONS:
+  -h  Show this message
+  -m  MeasurementSet directory
+  -p  Number of pipelines (default 1)
+EOF
+}
+
+ROOT=`pwd`
+SCONFIG=$ROOT/../src/xml/serverConfig.xml
+PCONFIG=$ROOT/../src/xml/pipelineConfig.xml
+PIPELINES=1
+MS=
+
+while getopts "hm:p:" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit 0
+      ;;
+    m)
+      MS=$OPTARG
+      ;;
+    p)
+      PIPELINES=$OPTARG
+      ;;
+    ?)
+      usage
+      exit 0
+      ;;
+  esac
+done
+
+if [[ -z $MS ]] || [[ ! -d $MS ]]
+then
+  usage
+  exit 1
 fi
 
 echo "Starting aartfaac-server"
-${ROOT}/aartfaac-server ${SCONFIG} &
+$ROOT/aartfaac-server $SCONFIG &
 sleep 2
 
-echo "Starting ${PIPELINES} aartfaac pipelines"
-for (( i=0; i<${PIPELINES}; i++ )); do
-	${ROOT}/aartfaac-pipeline ${PCONFIG} &
+echo "Starting $PIPELINES aartfaac pipelines"
+for (( i=0; i<$PIPELINES; i++ ))
+do
+  $ROOT/aartfaac-pipeline $PCONFIG $MS &
 done;
 sleep 5
 
 echo "Starting aartfaac emulator"
-${ROOT}/aartfaac-emulator $1 &
+$ROOT/aartfaac-emulator $MS &
 
 exit 0
