65027d1dada940d590e048198a2bfd4cc4962442
Error323 <folkerthuizinga@gmail.com>
2012-11-29 12:12:39 +0100

    Gridding WIP

--- src/pipeline/modules/imager/Imager.cpp
+++ src/pipeline/modules/imager/Imager.cpp
@@ -8,6 +8,8 @@
 #include <casacore/ms/MeasurementSets.h>
 #include <pelican/utility/Config.h>
 #include <QtCore>
+#include <fstream>
+#include <iostream>
 #include <limits>
 
 extern char *gTableName;
@@ -35,7 +37,7 @@ Imager::Imager(const ConfigNode &inConfig):
 
   for (int a1 = 0; a1 < NUM_ANTENNAS; a1++)
   {
-    for (int a2 = a1; a2 < NUM_ANTENNAS; a2++)
+    for (int a2 = 0; a2 < NUM_ANTENNAS; a2++)
     {
       casa::String a1_name = msc.antenna().name()(a1);
       casa::String a2_name = msc.antenna().name()(a2);
@@ -46,8 +48,6 @@ Imager::Imager(const ConfigNode &inConfig):
 
       mUCoords(a1, a2) = uvw.uvw[0];
       mVCoords(a1, a2) = uvw.uvw[1];
-      mUCoords(a2, a1) = uvw.uvw[0];
-      mVCoords(a2, a1) = uvw.uvw[1];
 
       minu = std::min<float>(minu, mUCoords(a1,a2));
       minv = std::min<float>(minv, mVCoords(a1,a2));
@@ -76,11 +76,32 @@ void Imager::run(const StreamBlob *input, StreamBlob *output)
   Q_UNUSED(output);
 
   gridding(input->mXX, input->mFlagged);
+
+  std::ofstream real, imag;
+  real.open(qPrintable(input->mDateTime.toString("dd-MM-yyyy_hh-mm-ss") + "-R.dat"));
+  imag.open(qPrintable(input->mDateTime.toString("dd-MM-yyyy_hh-mm-ss") + "-I.dat"));
+
+  for (int i = 0; i < IMAGE_OUTPUT_SIZE; i++)
+  {
+    for (int j = 0; j < IMAGE_OUTPUT_SIZE; j++)
+    {
+      float cr = mGridded(i, j).real();
+      float ci = mGridded(i, j).imag();
+      real << cr << " ";
+      imag << ci << " ";
+    }
+    real << std::endl;
+    imag << std::endl;
+  }
+  real.close();
+  imag.close();
 }
 
 void Imager::gridding(const MatrixXcf &inCorrelations, const std::vector<int> &inFlagged)
 {
   mGridded.setZero();
+
+  static const float sq2 = sqrtf(2.0f);
 
   for (int a1 = 0; a1 < NUM_ANTENNAS; a1++)
   {
@@ -93,39 +114,41 @@ void Imager::gridding(const MatrixXcf &inCorrelations, const std::vector<int> &i
         continue;
 
       const std::complex<float> &corr = inCorrelations(a1, a2);
-      float amplitude = std::abs(corr);
-      std::complex<float> phasor = corr / amplitude;
 
       float uidx = mUCoords(a1, a2);
       int uidxl = std::floor(uidx);
       int uidxh = std::ceil(uidx);
+
       Q_ASSERT(uidxl >= 0 && uidxl < IMAGE_OUTPUT_SIZE);
       Q_ASSERT(uidxh >= 0 && uidxh < IMAGE_OUTPUT_SIZE);
 
       float dul = std::abs(uidx - uidxl);
       float duh = std::abs(uidx - uidxh);
 
-      float sul = duh * amplitude;
-      float suh = dul * amplitude;
-
       float vidx = mVCoords(a1, a2);
       int vidxl = std::floor(vidx);
       int vidxh = std::ceil(vidx);
+
       Q_ASSERT(vidxl >= 0 && vidxl < IMAGE_OUTPUT_SIZE);
       Q_ASSERT(vidxh >= 0 && vidxh < IMAGE_OUTPUT_SIZE);
 
       float dvl = std::abs(vidx - vidxl);
       float dvh = std::abs(vidx - vidxh);
 
-      float sull = dvh * sul;
-      float suhl = dvh * suh;
-      float sulh = dvl * sul;
-      float suhh = dvl * suh;
+      float sull = (1.0f - (sqrtf(dul*dul + dvl*dvl) / sq2));
+      float suhl = (1.0f - (sqrtf(duh*duh + dvl*dvl) / sq2));
+      float sulh = (1.0f - (sqrtf(dul*dul + dvh*dvh) / sq2));
+      float suhh = (1.0f - (sqrtf(duh*duh + dvh*dvh) / sq2));
 
-      mGridded(uidxl, vidxl) += sull * phasor;
-      mGridded(uidxl, vidxh) += sulh * phasor;
-      mGridded(uidxh, vidxl) += suhl * phasor;
-      mGridded(uidxh, vidxh) += suhh * phasor;
+      Q_ASSERT(sull >= 0.0f && sull <= 1.0f);
+      Q_ASSERT(suhl >= 0.0f && suhl <= 1.0f);
+      Q_ASSERT(sulh >= 0.0f && sulh <= 1.0f);
+      Q_ASSERT(suhh >= 0.0f && suhh <= 1.0f);
+
+      mGridded(uidxl, vidxl) += sull * corr;
+      mGridded(uidxl, vidxh) += sulh * corr;
+      mGridded(uidxh, vidxl) += suhl * corr;
+      mGridded(uidxh, vidxh) += suhh * corr;
     }
   }
 }
