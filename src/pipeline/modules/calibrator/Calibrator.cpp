09a0c5eee9d0efdaa9b4c2b3c6ba1ee6ac88af4b
Error323 <folkerthuizinga@gmail.com>
2013-03-11 20:13:57 +0100

    Finished calibration

--- src/pipeline/modules/calibrator/Calibrator.cpp
+++ src/pipeline/modules/calibrator/Calibrator.cpp
@@ -4,7 +4,9 @@
 #include "../../../Constants.h"
 #include "../../../utilities/UVWParser.h"
 #include "../../../utilities/Utils.h"
+#include "../../../utilities/Simplex.h"
 
+#include <algorithm>
 #include <casacore/tables/Tables/TableParse.h>
 #include <casacore/ms/MeasurementSets.h>
 #include <pelican/utility/Config.h>
@@ -12,6 +14,7 @@
 #include <QDebug>
 
 extern char *gTableName;
+
 
 Calibrator::Calibrator(const ConfigNode &inConfig):
   AbstractModule(inConfig)
@@ -34,7 +37,7 @@ Calibrator::Calibrator(const ConfigNode &inConfig):
   mVCoords.resize(NUM_ANTENNAS, NUM_ANTENNAS);
   mUVDist.resize(NUM_ANTENNAS, NUM_ANTENNAS);
   mSpatialFilterMask.resize(NUM_ANTENNAS, NUM_ANTENNAS);
-  mOut.resize(NUM_ANTENNAS, NUM_ANTENNAS);
+  mNoiseCovMatrix.resize(NUM_ANTENNAS, NUM_ANTENNAS);
   QString uvw_file_name = inConfig.getOption("uvw", "path");
   UVWParser::Type lba_type = UVWParser::Type(inConfig.getOption("lba", "type").toInt());
   UVWParser uvw_parser(uvw_file_name);
@@ -58,6 +61,7 @@ Calibrator::Calibrator(const ConfigNode &inConfig):
 
   mRaSources.resize(4);
   mDecSources.resize(4);
+  mFluxes.resize(4);
 
   // Cassiopeia A
   mRaSources(0)   = 6.1138f;
@@ -87,12 +91,16 @@ Calibrator::~Calibrator()
 
 void Calibrator::run(const StreamBlob *input, StreamBlob *output)
 {
-  static const float min_restriction = 10.0f;   ///< avoid vis. below this wavelengths
-  static const float max_restriction = 60.0f;   ///< avoid vis. above this much meters
-  static const float lightspeed = 299792458.0f; ///< Speed of light in m/s
+  static const float min_restriction = 10.0f;   									 ///< avoid vis. below this wavelengths
+  static const float max_restriction = 60.0f;   									 ///< avoid vis. above this much meters
+  static const Vector3f normal(0.598753f, 0.072099f, 0.797682f); ///< Normal to CS002 (central antenna)
 
-  float uvdist_cutoff = std::min<float>(min_restriction*float(lightspeed/input->mFrequency), max_restriction);
+  mFrequency = input->mFrequency;
+  float uvdist_cutoff = std::min<float>(min_restriction*float(C_MS/mFrequency), max_restriction);
 
+  // =====================================
+  // ==== 0. Prepare/Reshape matrices ====
+  // =====================================
   int num_antennas = NUM_ANTENNAS - input->mFlagged.size();
 
   if (mFlagged.size() != input->mFlagged.size())
@@ -100,10 +108,9 @@ void Calibrator::run(const StreamBlob *input, StreamBlob *output)
     mNormalizedData.resize(num_antennas, num_antennas);
     mSpatialFilterMask.resize(num_antennas, num_antennas);
     mMask.resize(num_antennas, num_antennas);
-    mOut.resize(num_antennas, num_antennas);
+    mNoiseCovMatrix.resize(num_antennas, num_antennas);
     mAntennaITRFReshaped.resize(num_antennas, 3);
-    mCalibrations.resize(num_antennas);
-    mSigmas.resize(num_antennas);
+    mGains.resize(num_antennas);
   }
   mFlagged = input->mFlagged;
 
@@ -130,17 +137,67 @@ void Calibrator::run(const StreamBlob *input, StreamBlob *output)
 
   double time = input->mMJDTime / 86400.0 + 2400000.5;
 
-  // Whitening of the array covariance matrix for DOA estimation
+  // ========================================================================
+  // ==== 1. Whitening of the array covariance matrix for DOA estimation ====
+  // ========================================================================
   float sqrt_dot = mNormalizedData.diagonal().norm();
   mNormalizedData.array() /= sqrt_dot;
 
-  // Initial calibration
-  statCal(mNormalizedData, time, input->mFrequency, mMask, mCalibrations, mSigmas, mOut);
+  // ================================
+  // ==== 2. Initial calibration ====
+  // ================================
+  MatrixXf src_pos(mRaSources.rows(), 3);
+  utils::radec2itrf<float>(mRaSources, mDecSources, mEpoch, time, src_pos);
+  VectorXf up = src_pos * normal;
+  mSelection.resize((up.array() > 0.0f).count(), 3);
 
-  // Reconstruct the full ACM from the reshaped matrices
-  mNormalizedData.array() *= sqrt_dot;
-  mNormalizedData = (mCalibrations * mCalibrations.adjoint()).array() * (mNormalizedData.array() - mOut.array()).array();
+  for (int i = 0, j = 0, n = src_pos.rows(); i < n; i++)
+    if (up(i) > 0.0f)
+    {
+      mSelection.row(j) = src_pos.row(i);
+      j++;
+    }
 
+  statCal(mNormalizedData, mFrequency, mMask, mGains, mFluxes, mNoiseCovMatrix);
+
+  // ====================================
+  // ==== 3. WSF Position Estimation ====
+  // ====================================
+  Q_ASSERT(mSelection.rows() == mFluxes.rows());
+  MatrixXf selection((mFluxes.array() > 0.01f).count(), 3);
+  VectorXf fluxes(selection.rows());
+  for (int i = 0, j = 0, n = selection.rows(); i < n; i++)
+    if (mFluxes(i) > 0.01f)
+    {
+      selection.row(j) = mSelection.row(i);
+      fluxes(j) = mFluxes(i);
+      j++;
+    }
+
+  wsfSrcPos(mNormalizedData, mNoiseCovMatrix, mGains, mFrequency, selection);
+
+  // ==============================
+  // ==== 4. Final calibration ====
+  // ==============================
+  std::complex<float> i1(0.0f, 1.0f);
+  i1 *= 2.0f * M_PI * mFrequency / C_MS;
+  MatrixXcf A = (-i1 * (mAntennaITRFReshaped * selection.transpose())).array().exp();
+  MatrixXf inv_mask = (mNoiseCovMatrix.array().abs() > 0.0f).select(MatrixXf::Zero(mNoiseCovMatrix.rows(), mNoiseCovMatrix.cols()), 1.0f);
+  walsCalibration(A, mNormalizedData, fluxes, inv_mask, mGains, mFluxes, mNoiseCovMatrix);
+  mGains = (1.0f/mGains.array());
+  mGains.adjointInPlace();
+  mNormalizedData = (mGains * mGains.adjoint()).array() * (mNormalizedData.array() - mNoiseCovMatrix.array()).array();
+
+
+  // ===============================
+  // ==== 5. A-team subtraction ====
+  // ===============================
+  //MatrixXcf ATeam = A * mFluxes.asDiagonal() * A.adjoint();
+  //mNormalizedData.array() -= ATeam.array();
+
+  // ================================================================
+  // ==== 6. Reconstruct the full ACM from the reshaped matrices ====
+  // ================================================================
   for (int a1 = 0, _a1 = 0; a1 < NUM_ANTENNAS; a1++)
   {
     if (std::find(mFlagged.begin(), mFlagged.end(), a1) != mFlagged.end())
@@ -160,31 +217,15 @@ void Calibrator::run(const StreamBlob *input, StreamBlob *output)
 }
 
 void Calibrator::statCal(const MatrixXcf &inData,
-                         const double inTime,
                          const double inFrequency,
                          MatrixXf &ioMask,
                          VectorXcf &outCalibrations,
                          VectorXf &outSigmas,
                          MatrixXcf &outVisibilities)
 {
-  static const Vector3f normal(0.598753f, 0.072099f, 0.797682f); ///< Normal to CS002 (central antenna)
-  static const float lightspeed = 299792458.0f;                  ///< Speed of light in m/s
-
-  MatrixXf src_pos(mRaSources.rows(), 3);
-  utils::radec2itrf<float>(mRaSources, mDecSources, mEpoch, inTime, src_pos);
-  VectorXf up = src_pos * normal;
-  MatrixXf in_range_src_pos((up.array() > 0.0f).count(), 3);
-
-  for (int i = 0, j = 0, n = src_pos.rows(); i < n; i++)
-    if (up(i) > 0.0f)
-    {
-      in_range_src_pos.row(j) = src_pos.row(i);
-      j++;
-    }
-
   std::complex<float> i1(0.0f, 1.0f);
-  i1 *= 2.0f * M_PI * inFrequency / lightspeed;
-  MatrixXcf A = (-i1 * (mAntennaITRFReshaped * in_range_src_pos.transpose())).array().exp();
+  i1 *= 2.0f * M_PI * inFrequency / C_MS;
+  MatrixXcf A = (-i1 * (mAntennaITRFReshaped * mSelection.transpose())).array().exp();
 
   MatrixXcf KA(A.rows()*A.rows(), A.cols());
   utils::khatrirao<std::complex<float> >(A.conjugate(), A, KA);
@@ -392,4 +433,64 @@ int Calibrator::gainSolv(const MatrixXcf &inModel,
     qCritical("[%s] No convergence after %d iterations", __FUNCTION__, i);
 
   return i;
+}
+
+VectorXf gEigenValuesAbs;
+bool EigenSorter(const int a, const int b)
+{
+  return gEigenValuesAbs(a) > gEigenValuesAbs(b);
+}
+
+void Calibrator::wsfSrcPos(const MatrixXcf &inData,
+                           const MatrixXcf &inSigma1,
+                           const VectorXcf &inGains,
+                           const double inFreq,
+                                 MatrixXf &ioPositions)
+{
+  int nsrc = ioPositions.rows();
+  VectorXf elevation = ioPositions.col(2).array().asin();
+  VectorXf azimuth = ioPositions.col(1).array() / ioPositions.col(0).array();
+  for (int i = 0; i < nsrc; i++)
+    azimuth(i) = atan(azimuth(i));
+
+  ComplexEigenSolver<MatrixXcf> solver(inData);
+  gEigenValuesAbs = solver.eigenvalues().array().abs();
+  std::vector<int> indices(gEigenValuesAbs.size());
+  for (int i = 0, n = indices.size(); i < n; i++)
+    indices[i] = i;
+
+  std::sort(indices.begin(), indices.end(), EigenSorter);
+
+  MatrixXcf Es(solver.eigenvectors().rows(), nsrc);
+  MatrixXcf eigenvalues_abs(nsrc, nsrc);
+  eigenvalues_abs.setZero();
+  for (int i = 0; i < nsrc; i++)
+  {
+    eigenvalues_abs(i,i).real() = gEigenValuesAbs(indices[i]);
+    Es.col(i) = solver.eigenvectors().col(indices[i]);
+  }
+
+  MatrixXcf eye(nsrc, nsrc); eye.setIdentity();
+  MatrixXcf A = (eigenvalues_abs.array() - (inSigma1.diagonal().mean() * eye).array());
+  MatrixXcf W = (A * A).array() / eigenvalues_abs.array();
+  W = (W.array() != W.array()).select(0, W); // IEEE way of checking for NANs
+  MatrixXcf EsWEs = Es * W * Es.adjoint();
+  MatrixXcf T = (inGains.array().inverse());
+  MatrixXcf G = T.conjugate().asDiagonal().toDenseMatrix();
+  WSFCost wsf_cost(EsWEs, G, inFreq, mAntennaITRFReshaped);
+  vector<float> init(nsrc*2);
+  for (int i = 0; i < nsrc; i++)
+  {
+    init[i] = azimuth(i);
+    init[i+nsrc] = elevation(i);
+  }
+
+  init = BT::Simplex(wsf_cost, init);
+
+  for (int i = 0; i < nsrc; i++)
+  {
+    ioPositions(i, 0) = cosf(init[i]) * cosf(init[i+nsrc]);
+    ioPositions(i, 1) = sinf(init[i]) * cosf(init[i+nsrc]);
+    ioPositions(i, 2) = sinf(init[i+nsrc]);
+  }
 }
--- src/pipeline/modules/calibrator/Calibrator.h
+++ src/pipeline/modules/calibrator/Calibrator.h
@@ -4,6 +4,9 @@
 #include <pelican/core/AbstractModule.h>
 #include <eigen3/Eigen/Dense>
 #include <complex>
+#include <vector>
+#include <cmath>
+#include "../../../Constants.h"
 
 using namespace pelican;
 using namespace Eigen;
@@ -22,12 +25,11 @@ public:
 
 private:
   void statCal(const MatrixXcf &inData,
-               const double inTime,
                const double inFrequency,
-               MatrixXf &ioMask,
-               VectorXcf &outCalibrations,
-               VectorXf &outSigmas,
-               MatrixXcf &outVisibilities);
+                     MatrixXf &ioMask,
+                     VectorXcf &outCalibrations,
+                     VectorXf &outSigmas,
+                     MatrixXcf &outVisibilities);
 
   int gainSolv(const MatrixXcf &inModel,
                const MatrixXcf &inData,
@@ -42,6 +44,52 @@ private:
                             VectorXf  &outSourcePowers,
                             MatrixXcf &outNoiseCovMatrix);
 
+  void wsfSrcPos(const MatrixXcf &inData,
+                 const MatrixXcf &inSigma1,
+                 const VectorXcf &inGains,
+                 const double inFreq,
+                       MatrixXf &ioPositions);
+
+  class WSFCost
+  {
+  public:
+    WSFCost(const MatrixXcf &inW, const MatrixXcf &inG, const double inFreq, const MatrixXf &inP):
+      W(inW),
+      G(inG),
+      freq(inFreq),
+      P(inP)
+    {
+    }
+
+    float operator()(vector<float> theta)
+    {
+      const int nsrc = theta.size() / 2;
+      const int nelem = P.rows();
+
+      MatrixXf src_pos(nsrc, 3);
+      for (int i = 0; i < nsrc; i++)
+      {
+        src_pos(i, 0) = cosf(theta[i]) * cosf(theta[i+nsrc]);
+        src_pos(i, 1) = sinf(theta[i]) * cosf(theta[i+nsrc]);
+        src_pos(i, 2) = sinf(theta[i]+nsrc);
+      }
+
+      std::complex<float> i1(0.0f, 1.0f);
+      i1 *= 2.0f * M_PI * freq / C_MS;
+      MatrixXcf T = (-i1 * (P * src_pos.transpose())).array().exp();
+      MatrixXcf A = G * T;
+      MatrixXcf eye(nelem, nelem); eye.setIdentity();
+      MatrixXcf PAperp = eye.array() - (A * (A.adjoint() * A).inverse() * A.adjoint()).array();
+      return (PAperp * W).trace().real();
+    }
+
+  private:
+    const MatrixXcf &W;
+    const MatrixXcf &G;
+    const double freq;
+    const MatrixXf &P;
+  };
+
   /// Initialized in the constructor and const
   MatrixXf mAntennaITRF;
   MatrixXf mUCoords;
@@ -53,14 +101,16 @@ private:
 
   /// Changed when new antenna are (un)flagged
   std::vector<int> mFlagged;
-  MatrixXcf mNormalizedData;
   MatrixXf mSpatialFilterMask;
   MatrixXf mAntennaITRFReshaped;
   MatrixXf mMask;
-  MatrixXcf mOut;
+  MatrixXf mSelection;
+  MatrixXcf mNormalizedData;
+  MatrixXcf mNoiseCovMatrix;
 
-  VectorXcf mCalibrations;
-  VectorXf mSigmas;
+  VectorXcf mGains;
+  VectorXf mFluxes;
+  double mFrequency;
 };
 
 PELICAN_DECLARE_MODULE(Calibrator)
