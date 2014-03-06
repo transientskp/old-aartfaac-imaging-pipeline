247306fe8f3a91206c26bd9e1c5ad0318ddc0ccb
John Swinbank <swinbank@transientskp.org>
2014-03-06 14:37:48 +0100

    First bash at startup scripts for Docker

---
+++ scripts/start_emulator.py
@@ -0,0 +1,60 @@
+from string import Template
+import subprocess
+import tempfile
+import argparse
+import os
+
+EMULATOR_CMD = "aartfaac-emulator"
+TEMPLATE_XML = Template("""
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE pelican>
+
+<configuration version="1.0">
+    <StreamEmulator     name="${name}">
+        <connection     host="${host}" port="${port}" />
+        <measurementset name="${ms}" />
+        <emulator       packetInterval="${packet_interval}"/>
+    </StreamEmulator>
+</configuration>
+""".strip())
+
+def generate_xml(**kwargs):
+    """Returns a temporary file object"""
+    xmlfile = tempfile.NamedTemporaryFile()
+    xmlfile.write(TEMPLATE_XML.substitute(**kwargs)))
+    return xmlfile
+
+def read_environment(variable_name="AARTFAAC_SERVER_PORT"):
+    """Returns (host, port)"""
+    # If defined, we expect the environment variable to be set by Docker to
+    # something in the format "tcp://hostname:port".
+    if variable_name in os.environ:
+        return os.environ[variable_name][6:].split(":")
+    else:
+        return "localhost", "4100"
+
+def get_configuration():
+    """Rerturns a populated configuration"""
+    default_host, default_port = read_environment()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--stream-name", help="Stream name", default="O1")
+    parser.add_argument("--server-host", help="Server hostname", default=default_host)
+    parser.add_argument("--server-port", help="Server port", default=default_port)
+    parser.add_argument("--packet-interval", help="Packet interval (ms)", type=int default=10)
+    parser.add_argument("ms", help="MeasurementSet containing data to send")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    config = get_configuration()
+
+    # Sanity check: input data exists
+    assert(os.path.exists(config.ms))
+
+    xmlfile = generate_xml(
+        name=config.stream_name,
+        host=config.server_host,
+        port=config.server_port,
+        ms=config.ms,
+        packet_interval=config.packet_interval
+    )
+    subprocess.call([EMULATOR_CMD, xmlfile.name])
---
+++ scripts/start_server.py
@@ -0,0 +1,80 @@
+from string import Template
+from itertools import chain
+import subprocess
+import tempfile
+import argparse
+import os
+
+SERVER_CMD = "aartfaac-server"
+TEMPLATE_XML = Template("""
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE pelican>
+
+<configuration version="1.0">
+  <server>
+    <buffers>
+      <StreamBlob>
+        <buffer maxSize="${buffer_max_size}" maxChunkSize="${buffer_max_chunk_size}"/>
+      </StreamBlob>
+    </buffers>
+
+    <chunkers>
+      <StreamChunker name="${stream_chunker_name}">
+        <data type="StreamBlob"/>
+        <stream subbands="${subbands}"
+                numChannels="${num_channels}"
+                frequency="${start_frequency}"
+                width="${channel_width}" />
+        <connection host="${input_host}"
+                    port="${input_port}"
+                    timeout="${input_timeout}" />
+      </StreamChunker>
+    </chunkers>
+  </server>
+</configuration>
+""".strip())
+
+def generate_xml(**kwargs):
+    """Returns a temporary file object"""
+    xmlfile = tempfile.NamedTemporaryFile()
+    xmlfile.write(
+        TEMPLATE_XML.substitute(**kwargs)
+    )
+    return xmlfile
+
+def get_configuration():
+    """Rerturns a populated configuration"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--buffer-max-size", help="???", default=4294967296)
+    parser.add_argument("--buffer-max-chunk-size", help="???", default=4294967296)
+    parser.add_argument("--stream-chunker-name", help="StreamChunker name", default="Stream1")
+    parser.add_argument("--input-host", help="Address to listen for incoming data", default="0.0.0.0")
+    parser.add_argument("--input-port", help="Port to listen for incoming data", default="4100")
+    parser.add_argument("--input-timeout", help="???", default="4100")
+    parser.add_argument("num_channels", help="Number of channels", type=int)
+    parser.add_argument("start_frequency", help="Frequency of first channel (Hz)", type=float)
+    parser.add_argument("channel_width", help="Width of each channel (Hz)", type=float)
+    parser.add_argument("subband", help="Define a subband: <first channel>-<last channel> (inclusive)", type=lambda x: x.split('-'), nargs="*")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    config = get_configuration()
+
+    # Sanity check on subband definitions: no channels less than 0
+    # or greater than the maximum.
+    assert min(int(x) for x in chain(*config.subband)) >= 0
+    assert max(int(x) for x in chain(*config.subband)) < int(config.num_channels)
+
+    xmlfile = generate_xml(
+        buffer_max_size=config.buffer_max_size,
+        buffer_max_chunk_size=config.buffer_max_chunk_size,
+        stream_chunker_name=config.stream_chunker_name,
+        subbands=",".join("-".join(x) for x in config.subband),
+        num_channels=config.num_channels,
+        start_frequency=config.start_frequency,
+        channel_width=config.channel_width,
+        input_host=config.input_host,
+        input_port=config.input_port,
+        input_timeout=config.input_timeout
+    )
+    subprocess.call([SERVER_CMD, xmlfile.name])
