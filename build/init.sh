beb6923ecdf0333532da4528e9b4d2538e50a5b3
Error323 <folkerthuizinga@gmail.com>
2016-03-28 10:13:31 +0000

    init.sh: disable openmp

--- build/init.sh
+++ build/init.sh
@@ -12,10 +12,11 @@ fi
 
 git clean -xdf
 
-cmake .. -DCMAKE_BUILD_TYPE=Release 
-         -DCASACORE_ROOT=$INSTALL_DIR 
-         -DPELICAN_INCLUDE_DIR=$INSTALL_DIR/include/pelican 
-         -DPELICAN_LIBRARY=$INSTALL_DIR/lib/libpelican.so 
-         -DPELICAN_TESTUTILS_LIBRARY=$INSTALL_DIR/lib/libpelican-testutils.so 
-         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR 
-         -DEIGEN3_INCLUDE_DIR=$INSTALL_DIR/include/eigen3
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+         -DCASACORE_ROOT=$INSTALL_DIR \
+         -DPELICAN_INCLUDE_DIR=$INSTALL_DIR/include/pelican \
+         -DPELICAN_LIBRARY=$INSTALL_DIR/lib/libpelican.so \
+         -DPELICAN_TESTUTILS_LIBRARY=$INSTALL_DIR/lib/libpelican-testutils.so \
+         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+         -DEIGEN3_INCLUDE_DIR=$INSTALL_DIR/include/eigen3 \
+         -DENABLE_OPENMP=OFF
